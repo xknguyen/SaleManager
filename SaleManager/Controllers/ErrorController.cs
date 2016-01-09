c1f012ae5bd0a5c8688bf3adedba6730f24d48f0
Xuân Khoa <nguyenxuankhoactk35@gmail.com>
2016-01-09 14:46:26 +0700

    update

---
+++ SaleManager/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace SaleManager.Controllers
+{
+    public class ErrorController : BaseController
+    {
+        public ActionResult ErrorPage(string url)
+        {
+            ViewBag.Url = url;
+            return View();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public ActionResult AccessDeniedPage(string url)
+        {
+            ViewBag.Url = url;
+            return View();
+        }
+    }
+}
